5d6266a9b804e58593246d098535e50956d24e6f
afox <afox@gaig.com>
2021-01-06 15:00:04 -0500

    added iterative versiono of recursive fibonacci solution

--- fib.py
+++ fib.py
@@ -2,7 +2,7 @@
 # F0 = 1. F1 = 1.
 # Fn = F(n-1) + F(n-2)
 
-# Returns nth number from the Fibonacci sequence
+# Returns nth number from the Fibonacci sequence using recursion
 def fib(n):
     if n == 0:
         return 0
@@ -10,6 +10,25 @@ def fib(n):
         return 1
     return fib(n-1) + fib(n-2)
 
+# Iterative fibonacci using a stack
+def iterative_fib(num):
+    stack = [num]
+
+    value = 0
+    while len(stack) > 0:
+        n = stack.pop()
+        if n == 0:
+            pass
+        elif n == 1:
+            value = value + 1
+        else:
+            stack.append(n-1)
+            stack.append(n-2)
+
+    return value
+
 print(fib(8))
+print(iterative_fib(5))
+
 
 
