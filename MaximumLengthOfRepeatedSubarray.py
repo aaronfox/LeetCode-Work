0f55fa21a91e2ab7820cda08419600f5ed731136
aaronfox <aaron.m.fox3@gmail.com>
2021-01-11 18:45:06 -0500

    add maximum length of repeated subarray

---
+++ MaximumLengthOfRepeatedSubarray.py
@@ -0,0 +1,26 @@
+# URL: https://leetcode.com/problems/maximum-length-of-repeated-subarray
+class Solution:
+    def findLength(self, A: List[int], B: List[int]) -> int:
+        # If A[i] == B[j]: dp[i][j] = dp[i+1][j+1] + 1
+        # Invariant: answer for dp where i and j are larger is always calculated correctly  
+        # Dynamic programming approach
+        
+        # Can find max solution while passing through dp
+        max = 0
+        # Create empty table
+        dp = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+        for i in range(len(A) - 1, -1, -1):
+            for j in range(len(B) - 1, -1, -1):
+                if A[i] == B[j]:
+                    dp[i][j] = dp[i+1][j+1] + 1
+                    if dp[i][j] > max:
+                        max = dp[i][j]
+        
+        return max
+        # # Answer is largest value in table
+        # max = 0
+        # for i in range(len(A)):
+        #     for j in range(len(B)):
+        #         if dp[i][j] > max:
+        #             max = dp[i][j]
+        # return max
