52cce263a203fc0e0a4b4c2a6a5f488d8738a9cf
aaronfox <aaron.m.fox3@gmail.com>
2021-01-11 19:54:52 -0500

    fix LCS dp matrix pythonic intialization

--- LCS.py
+++ LCS.py
@@ -15,7 +15,8 @@
 # Else: dp[i][j] = 0
 
 def LCS(s1, s2):
-    dp = [[0] * (len(s1) + 1)] * (len(s2) + 1)
+    # dp = [[0] * (len(s1) + 1)] * (len(s2) + 1)
+    dp = [[0 for i in range(len(s1) + 1)] for j in range(len(s2) + 1)]
     max = 0
     for i in range(1, len(s1)):
         for j in range(1, len(s2)):
