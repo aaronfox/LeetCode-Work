8027c7fdee5bd27e00bcfbf9f805c0d74604c06e
aaronfox <aaron.m.fox3@gmail.com>
2021-03-23 15:43:19 -0400

    add another solution saving space complexity versus DP algo

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -1,10 +1,49 @@
 # URL: https://leetcode.com/problems/longest-palindromic-substring/
 class Solution:
+    
+    def expandAroundCenter(self, s, left, right):
+        # Take advantage of expanding around a center character
+        # and keep going outward until the the characters don't match
+        # Note: should check odd number of letter palindromes and
+        # even number of letter palindromes
+        # O(n^2) runtime complexity since expanding around center can take O(n) time as well
+        # O(1) space complexity
+        L = left
+        R = right
+        while L >= 0 and R < len(s) and s[L] == s[R]:
+            L -= 1
+            R += 1
+        
+        return R - L - 1
+    
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
+        # Avoiding DP, expand around center for every index
+        if len(s) <= 1:
+            return s
+        
+        start = 0
+        end = 0
+        for i in range(len(s)):
+            # For finding odd length palindromes like aba
+            len1 = self.expandAroundCenter(s, i, i)
+            # For finding even length palindromes like abba
+            len2 = self.expandAroundCenter(s, i, i + 1)
+            
+            length = max(len1, len2)
+            
+            # If a larger length is found,
+            # keep track of its respective start and end indices
+            # by setting start to the current index minus half the length
+            # and by setting end index to current index plus half the length
+            if length > end - start:
+                start = i - int((length - 1) / 2)
+                end = i + int(length / 2)
+                
+        return s[start:end + 1]
         
         # Dynamic Programming Solutions
         # Runs in O(n^2) time with O(n^2) space complexity to hold DP table
